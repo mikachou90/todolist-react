1f4ec6d258024d28d35124e9e8773b697e612bd7
mikachou <chou1005.tw@gmail.com>
2023-07-25 14:04:49 +0800

    first commit

---
+++ src/pages/TodoPage.jsx
@@ -0,0 +1,144 @@
+import {
+  Footer,
+  Header,
+  TodoCollection,
+  TodoInput,
+  TodoItem,
+} from 'components';
+import { useState } from 'react';
+
+const dummyTodos = [
+  {
+    title: 'Learn react-router',
+    isDone: true,
+    id: 1,
+  },
+  {
+    title: 'Learn to create custom hooks',
+    isDone: false,
+    id: 2,
+  },
+  {
+    title: 'Learn to use context',
+    isDone: true,
+    id: 3,
+  },
+  {
+    title: 'Learn to implement auth',
+    isDone: false,
+    id: 4,
+  },
+];
+
+const TodoPage = () => {
+  const [inputValue, setInputValue] = useState('');
+
+  const [todos, setTodos] = useState(dummyTodos);
+
+  const handleChange = (value) => {
+    setInputValue(value);
+  };
+
+  const handleAddTodo = () => {
+    //判斷若輸入空值則不做任何事
+    if (inputValue.length === 0) {
+      return;
+    }
+
+    setTodos((prevTodos) => {
+      return [
+        ...prevTodos,
+        {
+          id: Math.random() * 100,
+          title: inputValue,
+          isDone: false,
+        },
+      ];
+    });
+
+    //新增後,input欄內需變成空值
+    setInputValue('');
+  };
+
+  const handleKeyDown = () => {
+    if (inputValue.length === 0) {
+      return;
+    }
+
+    setTodos((prevTodos) => {
+      return [
+        ...prevTodos,
+        {
+          id: Math.random() * 100,
+          title: inputValue,
+          isDone: false,
+        },
+      ];
+    });
+
+    setInputValue('');
+  };
+
+  const handleToggleDone = (id) => {
+    setTodos((prevTodos) => {
+      return prevTodos.map((todo) => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            isDone: !todo.isDone,
+          };
+        }
+        //除了id相符的todo外，其他(沒變化)的todo要回傳回去
+        return todo;
+      });
+    });
+  };
+
+  const handleChangeMode = ({ id, isEdit }) => {
+    setTodos((prevTodos) => {
+      return prevTodos.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, isEdit };
+        }
+        return { ...todo, isEdit: false };
+      });
+    });
+  };
+
+  const handleSave = ({ id, title }) => {
+    setTodos((prevTodos) => {
+      return prevTodos.map((todo) => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            title,
+            isEdit: false,
+          };
+        }
+        return todo;
+      });
+    });
+  };
+
+  return (
+    <div>
+      TodoPage
+      <Header />
+      <TodoInput
+        inputValue={inputValue}
+        onChange={handleChange}
+        onAddTodo={handleAddTodo}
+        onKeyDown={handleKeyDown}
+      />
+      <TodoCollection
+        todos={todos}
+        onToggleDone={handleToggleDone}
+        onChangeMode={handleChangeMode}
+        onSave={handleSave}
+      />
+      <Footer />
+    </div>
+  );
+};
+
+export default TodoPage;
